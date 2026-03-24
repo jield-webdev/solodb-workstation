4e0a0e7ac6e4d961ae7c145e58997beb54ed374f
pedrofm <pedrofm@pedrofm.dev>
2026-03-24 13:00:10 +0100

    Use ScannerContext

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,10 @@
 import { type ReactElement } from "react";
 import { AuthProvider } from "../auth/context/AuthProvider.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  ScannerProvider,
+  NotificationProvider,
+} from "@jield/solodb-react-components";
 
 interface ProvidersProps {
   children: ReactElement;
@@ -11,7 +15,13 @@ const queryClient = new QueryClient();
 export function Providers({ children }: ProvidersProps) {
   return (
     <AuthProvider>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <NotificationProvider>
+        <ScannerProvider>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </ScannerProvider>
+      </NotificationProvider>
     </AuthProvider>
   );
 }
